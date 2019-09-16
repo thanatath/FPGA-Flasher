1b74393726e0c4afd36418fcfa67f6db98d5b891
Shi_ba'San <thanatuth.euro@gmail.com>
2019-09-16 18:31:42 +0700

    Big Update

--- FPGA-Flasher.bat
+++ FPGA-Flasher.bat
@@ -1,26 +1,33 @@
 @ECHO OFF
-REM BFCPEOPTIONSTART
-REM Advanced BAT to EXE Converter www.BatToExeConverter.com
-REM BFCPEEXE=
-REM BFCPEICON=
-REM BFCPEICONINDEX=-1
-REM BFCPEEMBEDDISPLAY=0
-REM BFCPEEMBEDDELETE=1
-REM BFCPEADMINEXE=0
-REM BFCPEINVISEXE=0
-REM BFCPEVERINCLUDE=0
-REM BFCPEVERVERSION=1.0.0.0
-REM BFCPEVERPRODUCT=FPGAFLAHSER
-REM BFCPEVERDESC=FPGAFLAHSER
-REM BFCPEVERCOMPANY=SHIBASAN
-REM BFCPEVERCOPYRIGHT=Copyright Info
-REM BFCPEOPTIONEND
+REM  QBFC Project Options Begin
+REM  HasVersionInfo: No
+REM  Companyname: 
+REM  Productname: 
+REM  Filedescription: 
+REM  Copyrights: 
+REM  Trademarks: 
+REM  Originalname: 
+REM  Comments: 
+REM  Productversion:  0. 0. 0. 0
+REM  Fileversion:  0. 0. 0. 0
+REM  Internalname: 
+REM  Appicon: ico.ico
+REM  AdministratorManifest: No
+REM  Embeddedfile: ftd2xx.dll
+REM  Embeddedfile: libusb0.dll
+REM  Embeddedfile: spiflasherLX9.bit
+REM  Embeddedfile: xc3sprog.exe
+REM  QBFC Project Options End
+ECHO ON
 @ECHO OFF
 :index
-SET bitfile="%1"
-if %bitfile%=="" goto error
+SET bitfile=%1
 if "%~x1" NEQ ".bit" goto error
 :con
+copy "%MYFILES%\xc3sprog.exe" "%~dp1"
+copy "%MYFILES%\ftd2xx.dll" "%~dp1"
+copy "%MYFILES%\libusb0.dll" "%~dp1"
+copy "%MYFILES%\spiflasherLX9.bit" "%~dp1"
 cls
 @ECHO OFF
 color E
@@ -35,19 +42,21 @@ echo ---------------------------------------------------------------------------
 echo -----------------------------
 echo File Bit From %bitfile%
 echo -----------------------------
-echo   1 For Normal Rom (Fast)
-echo   2 For PROM Rom (Slow)
+echo   1 For Normal Rom (Fast - Temporary)
+echo   2 For PROM Rom (Slow - Permanent)
 echo -----------------------------
 set /P INPUT=Type of Flash: %=%
 if %input%==1 goto normal
-if %input%==2 goto prom if NOT goto index
+if %input%==2 goto prom
+goto index
 :normal
-"%~dp0"xc3sprog.exe -c ftdi -L -v -p 0 %bitfile%
+xc3sprog.exe -c ftdi -L -v -p 0 "%bitfile%"
 PAUSE
 goto con
 :prom
-"%~dp0"xc3sprog.exe -c ftdi %cd%\spiflasherLX9.bit
-"%~dp0"xc3sprog.exe -c ftdi -L -v -p 0 -I %bitfile%
+xc3sprog.exe -c ftdi "%cd%\spiflasherLX9.bit"
+xc3sprog.exe -c ftdi -L -v -p 0 -I "%bitfile%"
+PAUSE
 cls
 echo -----------------------------------------------------------
 echo In Prom rom flashed. it's need to press reset on the board.
